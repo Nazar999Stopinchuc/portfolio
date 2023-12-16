399804ae9d8ed62faa685ba7f971d470c639ea68
Nazar999Stopinchuc <148930239+Nazar999Stopinchuc@users.noreply.github.com>
2023-12-15 20:13:13 -0500

    add files

---
+++ script/script.js
@@ -0,0 +1,41 @@
+
+
+
+$(function () {
+
+  let $page = $('html, body');
+  $('.menu__link, .logo').click(function () {
+    $page.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+    return false;
+  });
+
+  $('.burger, .menu__link').click(function () {
+    $('.burger, .menu__list').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+});
+
+window.onscroll = function headerFixed() {
+  let header = document.querySelector('.header__wrapper');
+
+  if (window.pageYOffset > 55) {
+    header.classList.add('header__wrapper--fixed');
+  } else {
+    header.classList.remove('header__wrapper--fixed');
+  }
+}
+
+
+var mixer = mixitup('.works__content');
+
+
+
+
+
+
+
+
+
